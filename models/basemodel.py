1634049962ad57ecfb3c638753bcf3547e5aa87a
Salvador E. Tropea <stropea@inti.gob.ar>
2025-05-26 18:31:44 -0300

    Commented dead code

--- models/basemodel.py
+++ models/basemodel.py
@@ -1,8 +1,8 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from transformers import Wav2Vec2Config, Wav2Vec2Model
-from transformers.modeling_outputs import BaseModelOutput
+# from transformers import Wav2Vec2Config, Wav2Vec2Model
+# from transformers.modeling_outputs import BaseModelOutput
 
 
 from torch import Tensor
