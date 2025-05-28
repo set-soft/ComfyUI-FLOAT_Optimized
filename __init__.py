c696e4a5c699b6ee40fdac2355a1b267a917a674
Salvador E. Tropea <salvador@inti.gob.ar>
2025-05-28 09:43:21 -0300

    Fixed spacing in __init__.py

--- __init__.py
+++ __init__.py
@@ -1,14 +1,14 @@
 from .nodes import LoadFloatModels, FloatProcess, FloatAdvancedParameters
- 
-NODE_CLASS_MAPPINGS = { 
-    "LoadFloatModelsOpt" : LoadFloatModels,
-    "FloatProcessOpt" : FloatProcess,
+
+NODE_CLASS_MAPPINGS = {
+    "LoadFloatModelsOpt": LoadFloatModels,
+    "FloatProcessOpt": FloatProcess,
     "FloatAdvancedParameters": FloatAdvancedParameters,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-     "LoadFloatModelsOpt" : "Load Float Models (Opt)",
-     "FloatProcessOpt" : "Float Process (Opt)",
+     "LoadFloatModelsOpt": "Load Float Models (Opt)",
+     "FloatProcessOpt": "Float Process (Opt)",
      "FloatAdvancedParameters": "Float Advanced Options",
 }
 
