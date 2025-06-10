93656a261458bc32b00927546242865f1483f017
Salvador E. Tropea <salvador@inti.gob.ar>
2025-06-10 13:31:06 -0300

    [Added] Mechanism to differentiate nodes level
    " (Ad)" are advanced
    " (VA)" are very advanced

--- __init__.py
+++ __init__.py
@@ -16,12 +16,22 @@ init_logger = logging.getLogger(f"{NODES_NAME}.__init__")
 NODE_CLASS_MAPPINGS = {}
 NODE_DISPLAY_NAME_MAPPINGS = {}
 
-for name, obj in inspect.getmembers(nodes) + inspect.getmembers(nodes_adv) + inspect.getmembers(nodes_vadv):
-    if not inspect.isclass(obj) or not hasattr(obj, "INPUT_TYPES"):
-        continue
-    assert hasattr(obj, "UNIQUE_NAME"), f"No name for {obj.__name__}"
-    NODE_CLASS_MAPPINGS[obj.UNIQUE_NAME] = obj
-    NODE_DISPLAY_NAME_MAPPINGS[obj.UNIQUE_NAME] = obj.DISPLAY_NAME
+
+def register_nodes(module):
+    suffix = " " + module.SUFFIX if hasattr(module, "SUFFIX") else ""
+    if suffix:
+        suffix = " " + suffix
+    for name, obj in inspect.getmembers(module):
+        if not inspect.isclass(obj) or not hasattr(obj, "INPUT_TYPES"):
+            continue
+        assert hasattr(obj, "UNIQUE_NAME"), f"No name for {obj.__name__}"
+        NODE_CLASS_MAPPINGS[obj.UNIQUE_NAME] = obj
+        NODE_DISPLAY_NAME_MAPPINGS[obj.UNIQUE_NAME] = obj.DISPLAY_NAME + suffix
+
+
+register_nodes(nodes)
+register_nodes(nodes_adv)
+register_nodes(nodes_vadv)
 
 init_logger.info(f"Registering {len(NODE_CLASS_MAPPINGS)} node(s).")
 init_logger.debug(f"{list(NODE_DISPLAY_NAME_MAPPINGS.values())}")
