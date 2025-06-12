bfe02d705f46b6ff1679d2d5d5b1420184d909f5
Salvador E. Tropea <salvador@inti.gob.ar>
2025-06-12 09:36:12 -0300

    Moved ODE constants to share them

--- utils/misc.py
+++ utils/misc.py
@@ -6,3 +6,13 @@
 NODES_NAME = "FLOAT_Optimized"
 NODES_DEBUG_VAR = NODES_NAME.upper() + "_NODES_DEBUG"
 EMOTIONS = ['none', 'angry', 'disgust', 'fear', 'happy', 'neutral', 'sad', 'surprise']
+# List of fixed-step solvers you from torchdiffeq
+TORCHDIFFEQ_FIXED_STEP_SOLVERS = [
+    "euler",
+    "midpoint",
+    "rk4",        # Classical Runge-Kutta 4th order
+    "heun2",      # Heun's method (RK2)
+    "heun3",      # Heun's method (RK3)
+    # "explicit_adams", # Fixed-step Adams-Bashforth (multi-step, might need more params like order)
+    # "implicit_adams", # Fixed-step Adams-Moulton (multi-step, implicit)
+]
